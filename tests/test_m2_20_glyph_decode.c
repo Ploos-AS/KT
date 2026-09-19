b62e5a920e103a91b625dd1905bced69d5ed0c82
pos <pgousdal@users.noreply.github.com>
2026-09-20 01:31:59 +0200

    Add M2.20 CP437 glyph corpus test

---
+++ tests/test_m2_20_glyph_decode.c
@@ -0,0 +1,12 @@
+#include "kt/terminal_screen.h"
+#include <assert.h>
+int main(void){
+ static const uint8_t b[]={0xDAu,0xC4u,0xBFu,0xB3u,0xC0u,0xD9u,0xC3u,0xB4u,0xC2u,0xC1u,0xC5u};
+ static const uint32_t u[]={0x250Cu,0x2500u,0x2510u,0x2502u,0x2514u,0x2518u,0x251Cu,0x2524u,0x252Cu,0x2534u,0x253Cu};
+ unsigned i;
+ for(i=0;i<sizeof b;i++)assert(kt_term_decode_codepoint(KT_TERM_PROFILE_CP437,b[i])==u[i]);
+ assert(kt_term_decode_codepoint(KT_TERM_PROFILE_ANSI,'A')==0x41u);
+ assert(kt_term_decode_codepoint(KT_TERM_PROFILE_PETSCII,'A')==0x41u);
+ assert(kt_term_decode_codepoint(KT_TERM_PROFILE_CP437,0xFFu)==0xFFFDu);
+ return 0;
+}
