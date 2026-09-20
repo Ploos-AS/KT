c413bfde99e394b6d92843272314921bfdac220d
pos <pgousdal@users.noreply.github.com>
2026-09-20 19:15:22 +0200

    Qualify M2.26 RGB24 conversion stride and guards

--- tests/test_m2_26_palette.c
+++ tests/test_m2_26_palette.c
@@ -14,5 +14,14 @@ int main(void){
  assert(kt_term_palette_for_profile(KT_TERM_PROFILE_CP437,&p)==0&&p==KT_TERM_PALETTE_ANSI16);
  assert(kt_term_palette_for_profile(KT_TERM_PROFILE_PETSCII,&p)==0&&p==KT_TERM_PALETTE_C64);
  assert(kt_term_palette_for_profile(99u,&p)==-1);
+ {uint8_t src[6]={0,15,0xaa,2,1,0xbb};uint8_t mem[16],*dst=&mem[1];unsigned i;for(i=0;i<16;i++)mem[i]=0xcc;
+  assert(kt_term_palette_convert_rgb24(KT_TERM_PALETTE_ANSI16,src,2,2,3,dst,7)==0);
+  assert(dst[0]==0&&dst[1]==0&&dst[2]==0&&dst[3]==255&&dst[4]==255&&dst[5]==255);
+  assert(dst[7]==0&&dst[8]==170&&dst[9]==0&&dst[10]==170&&dst[11]==0&&dst[12]==0);
+  assert(mem[0]==0xcc&&mem[15]==0xcc&&dst[6]==0xcc&&dst[13]==0xcc);
+  src[0]=16;assert(kt_term_palette_convert_rgb24(KT_TERM_PALETTE_ANSI16,src,2,2,3,dst,7)==-2);
+  assert(kt_term_palette_convert_rgb24(KT_TERM_PALETTE_ANSI16,src,2,2,1,dst,7)==-1);
+  assert(kt_term_palette_convert_rgb24(KT_TERM_PALETTE_ANSI16,src,2,2,3,dst,5)==-1);
+ }
  return 0;
 }
