f6d693213621cbd025093bd841c52c4d589c4ac2
pos <pgousdal@users.noreply.github.com>
2026-09-20 19:48:00 +0200

    Connect renderer damage to dirty row contract

--- include/kt/terminal_dirty.h
+++ include/kt/terminal_dirty.h
@@ -1,8 +1,11 @@
 #ifndef KT_TERMINAL_DIRTY_H
 #define KT_TERMINAL_DIRTY_H
 #include <stdint.h>
+#include "kt/terminal_renderer.h"
 typedef struct kt_term_dirty_rows { uint16_t first,count; uint8_t valid; } kt_term_dirty_rows;
 void kt_term_dirty_rows_reset(kt_term_dirty_rows *);
 int kt_term_dirty_rows_add_cell(kt_term_dirty_rows *,uint16_t cell_y,uint8_t cell_height,uint16_t fb_height);
 int kt_term_dirty_rows_add_cursor(kt_term_dirty_rows *,uint16_t old_y,uint16_t new_y,uint8_t cell_height,uint16_t fb_height);
+int kt_term_dirty_rows_from_damage(kt_term_dirty_rows *,const kt_term_render_damage *,
+                                   uint8_t cell_height,uint16_t fb_height);
 #endif
