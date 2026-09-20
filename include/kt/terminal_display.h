b4a053d31af499d79000afe2f9b9c11ca888405b
pos <pgousdal@users.noreply.github.com>
2026-09-20 19:18:49 +0200

    Start M2.27 terminal display presentation contract

---
+++ include/kt/terminal_display.h
@@ -0,0 +1,24 @@
+#ifndef KT_TERMINAL_DISPLAY_H
+#define KT_TERMINAL_DISPLAY_H
+#include <stddef.h>
+#include <stdint.h>
+
+#define KT_TERM_DISPLAY_INDEXED8 0u
+#define KT_TERM_DISPLAY_RGB24    1u
+
+typedef struct kt_term_display_frame {
+ const uint8_t *pixels;
+ uint16_t width,height;
+ size_t stride;
+ uint8_t format;
+ uint8_t palette;
+} kt_term_display_frame;
+
+typedef struct kt_term_display_ops {
+ int (*begin)(void *,const kt_term_display_frame *);
+ int (*present_rows)(void *,const kt_term_display_frame *,uint16_t,uint16_t);
+ int (*end)(void *,const kt_term_display_frame *);
+} kt_term_display_ops;
+
+int kt_term_display_present(const kt_term_display_ops *,void *,const kt_term_display_frame *);
+#endif
