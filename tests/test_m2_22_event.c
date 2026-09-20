bcdc4cd6186144a7353f9fbbe0e6deefebbb5741
pos <pgousdal@users.noreply.github.com>
2026-09-20 09:01:02 +0200

    Add M2.22 terminal event dispatcher test

---
+++ tests/test_m2_22_event.c
@@ -0,0 +1,19 @@
+#include "kt/terminal_event.h"
+#include <assert.h>
+static unsigned n,b,p,r,k,m;static uint8_t last;static uint32_t key;
+static void cb(void*c,uint8_t x){(void)c;n++;last=x;}
+static void cp(void*c,const uint8_t*d,uint16_t z){(void)c;(void)d;p=z;}
+static void cr(void*c,uint16_t w,uint16_t h){(void)c;r=w+h;}
+static void ck(void*c,uint32_t x,uint8_t q){(void)c;k=x;m=q;}
+static void cm(void*c,uint16_t x,uint16_t y,uint8_t q,uint8_t z){(void)c;b=x+y+q+z;}
+int main(void){
+ kt_term_input_ops o={cb,cp,cr,ck,cm};kt_term_event e={KT_TERM_EVENT_BYTE,0x41,0,0,0,0,0,0};
+ assert(kt_term_dispatch_event(&e,&o,0)==0&&n==1u&&last==0x41u);
+ e.type=KT_TERM_EVENT_PASTE;e.data=(const uint8_t*)"abc";e.data_len=3;assert(kt_term_dispatch_event(&e,&o,0)==0&&p==3u);
+ e.type=KT_TERM_EVENT_RESIZE;e.width=80;e.height=25;assert(kt_term_dispatch_event(&e,&o,0)==0&&r==105u);
+ e.type=KT_TERM_EVENT_KEY;e.key=0x1234u;e.mods=2;assert(kt_term_dispatch_event(&e,&o,0)==0&&k==0x1234u&&m==2u);
+ e.type=KT_TERM_EVENT_MOUSE;e.width=4;e.height=5;e.byte=1;e.mods=2;assert(kt_term_dispatch_event(&e,&o,0)==0&&b==12u);
+ e.type=99;assert(kt_term_dispatch_event(&e,&o,0)==-3);
+ assert(kt_term_dispatch_event(0,&o,0)==-1);
+ return 0;
+}
