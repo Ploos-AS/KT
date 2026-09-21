8a4837e40ce0a864562dc4333205ddd2902215a7
pos <pgousdal@users.noreply.github.com>
2026-09-21 17:21:50 +0200

    Start M2.33 terminal resize orchestration contract

---
+++ include/kt/terminal_resize.h
@@ -0,0 +1,18 @@
+#ifndef KT_TERMINAL_RESIZE_H
+#define KT_TERMINAL_RESIZE_H
+#include <stdint.h>
+#include "kt/terminal_session.h"
+#include "kt/terminal_dirty.h"
+#include "kt/terminal_scheduler.h"
+
+typedef struct kt_term_resize_result {
+ uint16_t old_cols,old_rows;
+ uint16_t new_cols,new_rows;
+ kt_term_dirty_rows dirty;
+ uint8_t changed;
+} kt_term_resize_result;
+
+int kt_term_resize_apply(kt_term_session *,kt_term_present_scheduler *,
+                         uint8_t,uint16_t,kt_term_resize_result *);
+
+#endif
