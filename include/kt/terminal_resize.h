e36d29414f8332f79aa02d4907eb89c019a99398
pos <pgousdal@users.noreply.github.com>
2026-09-21 17:32:11 +0200

    Add transactional geometry policy resize API

--- include/kt/terminal_resize.h
+++ include/kt/terminal_resize.h
@@ -14,5 +14,8 @@ typedef struct kt_term_resize_result {
 
 int kt_term_resize_apply(kt_term_session *,kt_term_present_scheduler *,
                          uint8_t,uint16_t,kt_term_resize_result *);
+int kt_term_resize_set_policy(kt_term_session *,kt_term_present_scheduler *,
+                              kt_term_geometry_policy,uint8_t,uint16_t,
+                              kt_term_resize_result *);
 
 #endif
