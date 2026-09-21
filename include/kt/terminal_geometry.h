4bfe79d52863a1708866f136f40db0d77bd4851a
pos <pgousdal@users.noreply.github.com>
2026-09-21 05:52:23 +0200

    Start M2.31 terminal geometry policy

---
+++ include/kt/terminal_geometry.h
@@ -0,0 +1,27 @@
+#ifndef KT_TERMINAL_GEOMETRY_H
+#define KT_TERMINAL_GEOMETRY_H
+#include <stdint.h>
+
+typedef enum kt_term_geometry_policy {
+ KT_TERM_GEOMETRY_FIXED=0,
+ KT_TERM_GEOMETRY_VIEWPORT=1,
+ KT_TERM_GEOMETRY_REMOTE=2
+} kt_term_geometry_policy;
+
+typedef struct kt_term_geometry {
+ uint16_t cols,rows;
+ uint16_t fixed_cols,fixed_rows;
+ uint16_t viewport_cols,viewport_rows;
+ uint16_t remote_cols,remote_rows;
+ uint8_t remote_valid;
+ kt_term_geometry_policy policy;
+} kt_term_geometry;
+
+int kt_term_geometry_init(kt_term_geometry *,uint16_t,uint16_t);
+int kt_term_geometry_set_policy(kt_term_geometry *,kt_term_geometry_policy);
+int kt_term_geometry_set_fixed(kt_term_geometry *,uint16_t,uint16_t);
+int kt_term_geometry_set_viewport(kt_term_geometry *,uint16_t,uint16_t);
+int kt_term_geometry_set_remote(kt_term_geometry *,uint16_t,uint16_t);
+void kt_term_geometry_clear_remote(kt_term_geometry *);
+
+#endif
