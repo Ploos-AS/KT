fa8d71d085b66e9cbf8a5b21fb2084ef93ac17b8
pos <pgousdal@users.noreply.github.com>
2026-09-21 14:37:30 +0200

    Bind M2.32 session resize to renderer cache

--- include/kt/terminal_session.h
+++ include/kt/terminal_session.h
@@ -4,16 +4,19 @@
 #include <stdint.h>
 #include "kt/terminal_geometry.h"
 #include "kt/terminal_screen.h"
+#include "kt/terminal_renderer.h"
 
 typedef struct kt_term_session {
  kt_term_geometry *geometry;
  kt_term_screen *screen;
  kt_term_cell *cells;
  size_t cell_capacity;
+ kt_term_render_cache *render_cache;
 } kt_term_session;
 
 int kt_term_session_init(kt_term_session *,kt_term_geometry *,kt_term_screen *,
                          kt_term_cell *,size_t);
 int kt_term_session_apply_geometry(kt_term_session *);
+void kt_term_session_bind_render_cache(kt_term_session *,kt_term_render_cache *);
 
 #endif
