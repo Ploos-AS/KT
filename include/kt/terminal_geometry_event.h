c2ec7a87c3aff6bd9b90b806435f2ad28e97c695
pos <pgousdal@users.noreply.github.com>
2026-09-22 06:41:30 +0200

    Add M2.34 resize adapter contract

--- include/kt/terminal_geometry_event.h
+++ include/kt/terminal_geometry_event.h
@@ -2,6 +2,7 @@
 #define KT_TERMINAL_GEOMETRY_EVENT_H
 #include <stdint.h>
 #include "kt/terminal_geometry.h"
+#include "kt/terminal_resize.h"
 
 typedef struct kt_term_geometry_event {
  kt_term_geometry_source source;
@@ -18,6 +19,21 @@ typedef struct kt_term_geometry_event_ops {
 } kt_term_geometry_event_ops;
 
 void kt_term_geometry_event_state_reset(kt_term_geometry_event_state *);
+typedef struct kt_term_geometry_resize_adapter {
+ kt_term_session *session;
+ kt_term_present_scheduler *scheduler;
+ uint8_t cell_height;
+ uint16_t fb_height;
+ kt_term_resize_result *result;
+} kt_term_geometry_resize_adapter;
+
+void kt_term_geometry_resize_adapter_init(kt_term_geometry_resize_adapter *,
+                                          kt_term_session *,
+                                          kt_term_present_scheduler *,
+                                          uint8_t,uint16_t,
+                                          kt_term_resize_result *);
+const kt_term_geometry_event_ops *kt_term_geometry_resize_adapter_ops(void);
+
 int kt_term_geometry_event_emit(kt_term_geometry_event_state *,
                                 const kt_term_geometry_event_ops *,void *,
                                 kt_term_geometry_source,uint16_t,uint16_t);
