e2d9292a69c80159f4ea77bd40da961391b217d3
pos <pgousdal@users.noreply.github.com>
2026-09-25 20:27:18 +0200

    Add M2.39 geometry state snapshot API

--- include/kt/terminal_geometry_session.h
+++ include/kt/terminal_geometry_session.h
@@ -32,4 +32,16 @@ int kt_term_geometry_session_connect_ssh(kt_term_geometry_session *,
 int kt_term_geometry_session_activate_viewport(kt_term_geometry_session *);
 int kt_term_geometry_session_connect_viewport(kt_term_geometry_session *,
                                               const kt_term_viewport *);
+typedef struct kt_term_geometry_snapshot {
+ kt_term_geometry_policy policy;
+ uint16_t screen_cols,screen_rows;
+ uint16_t viewport_cols,viewport_rows;
+ uint16_t remote_cols,remote_rows;
+ uint8_t remote_valid;
+ uint8_t remote_source_valid;
+ kt_term_geometry_source remote_source;
+} kt_term_geometry_snapshot;
+
+int kt_term_geometry_session_snapshot(const kt_term_geometry_session *,
+                                      kt_term_geometry_snapshot *);
 #endif
